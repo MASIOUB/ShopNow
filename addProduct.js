c185dbf66cdebc895efc04d80011d20d1bf8c0c9
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-01-13 13:47:45 +0100

    update js file of first page

--- addProduct.js
+++ addProduct.js
@@ -1,14 +1,14 @@
 const main = document.querySelector("main");
 const form = document.querySelector("form");
-// const div = document.querySelector("#div");
+const div = document.querySelector("#div");
 
 
-// var erreur;
+var erreur;
 
-// let reference = document.querySelector("#reference");
-// let nom = document.querySelector("#name");
-// let category = document.querySelector("#category");
-// let quantity = document.querySelector("#quantity");
+let reference = document.querySelector("#reference");
+let nom = document.querySelector("#name");
+let category = document.querySelector("#category");
+let quantity = document.querySelector("#quantity");
 
 const popup = document.createElement("div");
 popup.classList.add("popup");
@@ -17,22 +17,25 @@ popup.classList.add("popup");
 const overlay = document.createElement("div");
 overlay.classList.add("overlay");
 
-// const paragraph = document.createElement("p");
-// paragraph.classList.add("erreur");
+const paragraph = document.createElement("p");
+paragraph.classList.add("erreur");
 
 
-// function erreurMessage() {
-//     erreur = "";
-//     erreur = "Something is wrong! Please check your data.";
-//     paragraph.innerText = erreur;
-//     div.appendChild(paragraph);
-// }
+function erreurMessage() {
+    erreur = "";
+    erreur = "Something is wrong! Please check your data.";
+    paragraph.innerText = erreur;
+    div.appendChild(paragraph);
+}
 
 function popupReselt() {
     popup.innerText = "Well regester"
     main.appendChild(popup);
     main.appendChild(overlay);
-    popup.append(button)
+    popup.append(button);
+    if(div.contains(paragraph)){
+        paragraph.remove();
+    }
 }
 
 const button = document.createElement("button");
@@ -45,11 +48,11 @@ button.addEventListener("click", () => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // if(!reference.value || !category.value || !quantity.value || nom.value){
-    //     erreurMessage();
-    // }
-    // else{
+    if(reference.value == "" || category.value == "" || quantity.value == "" || nom.value == ""){
+        erreurMessage();
+    }
+    else{
         popupReselt();
-    // }   
+    }   
 })
 
